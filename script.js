14425230792f70e7c8252eaf708d74caf8253a94
DenisOduor <denisoduor732@gmail.com>
2025-05-31 10:20:42 +0000

    final project

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const faqItems = document.querySelectorAll('.faq-item');
+
+  faqItems.forEach(item => {
+    const question = item.querySelector('.question');
+    const answer = item.querySelector('.answer');
+
+    question.addEventListener('click', () => {
+      if (answer.style.display === 'block') {
+        answer.style.display = 'none';
+      } else {
+        answer.style.display = 'block';
+      }
+    });
+  });
+});
